a3d3fa5ebc6e9aa67179056733a94b8049ce74ba
YAlejandroG <yalejandro.gomez@gmail.com>
2019-10-29 21:38:46 +0000

    Ingresar valor de radio

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void){
+    int R;
+    std::count<<"Ingresar radio"<<std::endl;
+    std::cin>>R;
+    std::cout<<"Ha ingresado un valor de radio R = "<<R<<std::endl;
+}
