479816afc2bc4b1d9b6a327363ee76bdaedba1bb
YAlejandroG <y.alejandro.gomez@gmail.com>
2019-10-29 22:36:19 +0000

    Ingresar primer lado

---
+++ 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main(void){
+    float l1;
+    std::cout<<"Ingresar primer lado"<<std::endl;
+    std::cin>>l1;
+    std::cout<<"Lado 1 = "<<l1<<std::endl;
+}
