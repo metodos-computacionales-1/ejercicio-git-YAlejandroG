5d1647c0ef3518bdad2d99a7c0aa484d09bb73cd
YAlejandroG <y.alejandro.gomez@gmail.com>
2019-10-29 22:37:42 +0000

    Ingresar valor segundo lado

--- 2.cpp
+++ 2.cpp
@@ -6,4 +6,8 @@ int main(void){
     std::cout<<"Ingresar primer lado"<<std::endl;
     std::cin>>l1;
     std::cout<<"Lado 1 = "<<l1<<std::endl;
+    float l2;
+    std::cout<<"Ingresar segundo lado"<<std::endl;
+    std::cin>>l2;
+    std::cout<<"Lado 2 = "<<l2<<std::endl;
 }
